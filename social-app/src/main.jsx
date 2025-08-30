ed4eb28f5ba6e674d86b058cd6a308a0caa4bc42
Aya Okasha <ayaskasha219@gmail.com>
2025-08-30 22:07:09 +0300

    bulid

--- social-app/src/main.jsx
+++ social-app/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.jsx";
-import Hero from "./hero";
 import { HeroUIProvider } from "@heroui/react";
 import CounterContextProvider from "./Context/CounterContext.jsx";
 import AuthContextProvider from "./Context/AuthContext.jsx";
